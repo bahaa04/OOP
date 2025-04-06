873c47304d00937395be7bf96358f553e48a9e3f
Bahaa <fellahbaha2005@gmail.com>
2025-04-06 22:30:04 +0100

    abstraction

---
+++ tp 03/ex01/src/shape.java
@@ -0,0 +1,19 @@
+abstract class shape {
+    protected double p;
+    protected double a;
+    final String name;
+
+    public shape(double P, double S,String name) {
+        this.p = P;
+        this.a = S;
+        this.name=name;
+    }
+
+    abstract public String getName() ;
+
+    abstract public double getP() ;
+
+    abstract public double getS() ;
+
+    abstract public void display() ;
+}
