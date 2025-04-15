b246005f635b2937f1d7c541935a12ef9e423025
Bahaa <fellahbaha2005@gmail.com>
2025-04-15 08:56:30 +0100

    fixage

--- tp 04/src/divisionBy0Exception.java
+++
@@ -1,8 +0,0 @@
-public class divisionBy0Exception extends Exception{
-
-    @Override
-    public void printStackTrace() {
-        System.out.println("division by zero is impossible");
-    }
-    
-}
